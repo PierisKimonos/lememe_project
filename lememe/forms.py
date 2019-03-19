a9f4ed74e0552c597cfa07f8e864778f40b3833f
Pieris Kimonos <2319593k@student.gla.ac.uk>
2019-03-19 21:35:43 +0000

    Fixed settings and change password form

--- lememe/forms.py
+++ lememe/forms.py
@@ -1,23 +1,10 @@
 from django import forms
 from django.contrib.auth.models import User
 from lememe.models import UserProfile, Post, Comment, Preference, Category
-from django.contrib.auth.forms import UserChangeForm
+from django.contrib.auth.forms import UserChangeForm, PasswordChangeForm
 
-
-# class CategoryForm(forms.ModelForm):
-#     name = forms.CharField(max_length=Category.max_length, help_text="Please enter the category name.")
-#     views = forms.IntegerField(widget=forms.HiddenInput(), initial=0)
-#     likes = forms.IntegerField(widget=forms.HiddenInput(), initial=0)
-#     slug = forms.CharField(widget=forms.HiddenInput(), required=False)
-#
-#     # An inline class to provide additional information on the form.
-#     class Meta:
-#         # Provide an association between the ModelForm and a model
-#         model = Category
-#         fields = ('name',)
 
 class CommentForm(forms.ModelForm):
-
     class Meta:
         model = Comment
         fields = ("text",)
@@ -39,14 +26,13 @@ class PostForm(forms.ModelForm):
     # views = forms.IntegerField(widget=forms.HiddenInput(), initial=0)
 
     class Meta:
-
         # Provide an association between the ModelForm and a model
         model = Post
         # What fields do we want to include in our form?
         # This way we don't need every field in the model present.
         # Some fields may allow NULL values, so we may not want to include them.
         # exclude = ["category",]
-        exclude = ('client_id','id','date','user','views')
+        exclude = ('client_id', 'id', 'date', 'user', 'views')
         # exclude = []
         # fields = (
         #     'title',
@@ -55,8 +41,6 @@ class PostForm(forms.ModelForm):
         # )
         # or specify the fields to include (i.e. not include the category field)
         # fields = ('title', 'url', 'views')
-
-
 
 
 class UserForm(forms.ModelForm):
@@ -97,3 +81,42 @@ class UpdateUserProfileSettingsForm(UserChangeForm):
             'website',
             'bio',
         ]
+
+
+class PasswordChangeCustomForm(PasswordChangeForm):
+    error_css_class = 'has-error'
+    error_messages = {'password_incorrect':
+                          "Your old password is incorrect. Try again."
+                      }
+    old_password = forms.CharField(required=True, label='Old Password',
+                                   widget=forms.PasswordInput(attrs={
+                                       'class': 'form-control'},
+                                       render_value=True),
+                                   error_messages={
+                                       'required': 'Old password field cannot be blank'})
+
+    new_password1 = forms.CharField(required=True, label='New Password',
+                                    widget=forms.PasswordInput(attrs={
+                                        'class': 'form-control'},
+                                        render_value=True),
+                                    error_messages={
+                                        'required': 'Old password field cannot be blank'})
+    new_password2 = forms.CharField(required=True, label='Confirm New Password',
+                                    widget=forms.PasswordInput(attrs={
+                                        'class': 'form-control'},
+                                        render_value=True),
+                                    error_messages={
+                                        'required': 'Old password field cannot be blank'})
+
+    def clean(self):
+        cd = self.cleaned_data
+        if cd.get('new_password1') != cd.get('new_password2'):
+            self.add_error('new_password2', "passwords do not match !")
+            # raise forms.ValidationError('password mismatch')
+        return cd
+
+    def clean_new_password2(self):
+        password2 = self.cleaned_data.get("new_password2", "")
+        if not password2:  # If both passwords has value
+            raise forms.ValidationError(_(u"Passwords can't be blank."))
+        return password2
